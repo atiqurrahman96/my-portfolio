6f32c41c8d689a18af43de8a5623c6a7ce57ed44
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-06-11 09:18:51 +0600

    navbar added

--- src/shared/About.js
+++ src/shared/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import image from '../assets/image/person.png'
 const About = () => {
     return (
         <div>
-            <h1>this is about page</h1>
+            <div class="card card-compact mx-auto w-96 bg-base-100 shadow-2xl mt-5 bg-gray-900">
+                <figure><img src={image} alt="Shoes" /></figure>
+                <div class="card-body">
+                    <h2 class="text-center text-xl
+                    font-bold text-white">This is Atiqur Rahman</h2>
+                    <p className='text-white'>I am from Kishorejong, Dhaka. I have completed my graduation from <strong>Islamic University, Kustia, Bangladesh</strong> under the department of <strong>English</strong>. I am a Jr Web Developer. As a developer, I would love to code. I always try to discover something new and also try to gather new knowledge about web development. I am always trying my best to improve my skills day by day.</p>
+                    <h3 className='text-xl text-white'>Phone:(+880) 1632741385</h3>
+                    <h4 className='text-xl text-white'>Email: atiqurrahmanbd302@gmail.com</h4>
+                </div>
+            </div>
         </div>
     );
 };
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -6,25 +6,25 @@ const Header = () => {
 
 
         <NavLink className='mx-3' to='/home'>Home</NavLink>
-        <NavLink className='mx-3' to='/about'>About</NavLink>
+        <NavLink className='mx-3' to='/about'>About ME</NavLink>
         <Link className='mx-3' to='/blog'>Blog</Link>
 
 
     </>
     return (
         <div>
-            <div class="navbar bg-base-100">
+            <div class="navbar bg-gray-900">
                 <div class="navbar-start">
                     <div class="dropdown">
-                        <label tabindex="0" class="btn btn-ghost btn-circle">
+                        <label tabindex="0" class="btn btn-white btn-circle lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
-                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-white rounded-box w-52">
                             {menuItems}
                         </ul>
                     </div>
                 </div>
-                <div class="navbar-center">
+                <div class="navbar-end hidden lg:block text-white">
                     {menuItems}
                 </div>
 
